acf16c134b0c81631d224f1946e1b607a5828a85
Tien25012003 <21520112@gm.uit.edu.vn>
2023-07-19 19:25:11 +0700

    Complete BottomTab

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:travel_ui/utilites/app_color.dart';
 import 'package:travel_ui/widgets/destination_carousel.dart';
+import 'package:travel_ui/widgets/hotel_carousel.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -12,6 +13,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   int _selectedIndex = 0;
+  int _currentTab = 0;
   final List<IconData> _icons = [
     FontAwesomeIcons.plane,
     FontAwesomeIcons.bed,
@@ -48,30 +50,95 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-            child: ListView(
-      padding: const EdgeInsets.symmetric(vertical: 30),
-      children: [
-        const Padding(
-          padding: EdgeInsets.only(left: 20, right: 50),
-          child: Text(
-            "What would you like to find?",
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.w500,
+      body: SafeArea(
+          child: ListView(
+        padding: const EdgeInsets.symmetric(vertical: 30),
+        children: [
+          const Padding(
+            padding: EdgeInsets.only(left: 20, right: 50),
+            child: Text(
+              "What would you like to find?",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w500,
+              ),
             ),
           ),
+          const SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: _icons.asMap().entries.map((e) {
+              return _buildIcon(e.key);
+            }).toList(),
+          ),
+          const SizedBox(height: 20),
+          const DestinationCarousel(),
+          const SizedBox(height: 10),
+          const HotelCarousel(),
+        ],
+      )),
+      bottomNavigationBar: Theme(
+        data: ThemeData(
+          splashColor: Colors.transparent,
+          highlightColor: Colors.transparent,
         ),
-        const SizedBox(height: 20),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: _icons.asMap().entries.map((e) {
-            return _buildIcon(e.key);
-          }).toList(),
+        child: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          currentIndex: _currentTab,
+          items: List<BottomNavigationBarItem>.generate(3, (index) {
+            if (index != 2) {
+              return BottomNavigationBarItem(
+                  icon: index == 0
+                      ? const Icon(Icons.search, size: 30)
+                      : const Icon(Icons.local_pizza),
+                  label: "");
+            } else {
+              return const BottomNavigationBarItem(
+                icon: CircleAvatar(
+                  radius: 15,
+                  backgroundImage: NetworkImage(
+                      "https://i1.wp.com/www.globalgranary.life/wp-content/uploads/2018/02/JONGSUK2017ygstage.jpg?resize=300%2C379&ssl=1"),
+                ),
+                label: "",
+              );
+            }
+          }),
+          onTap: (int index) {
+            setState(() {
+              _currentTab = index;
+            });
+          },
+          // items: const [
+          //   BottomNavigationBarItem(
+          //     icon: Icon(
+          //       Icons.search,
+          //       size: 30,
+          //       color: Colors.black,
+          //     ),
+          //     label: "",
+          //   ),
+          //   BottomNavigationBarItem(
+          //     icon: Icon(
+          //       Icons.local_pizza,
+          //       size: 30,
+          //       color: Colors.black,
+          //     ),
+          //     label: "",
+          //   ),
+          //   BottomNavigationBarItem(
+          //     icon: CircleAvatar(
+          //       radius: 15,
+          //       backgroundImage: NetworkImage(
+          //           "https://i1.wp.com/www.globalgranary.life/wp-content/uploads/2018/02/JONGSUK2017ygstage.jpg?resize=300%2C379&ssl=1"),
+          //     ),
+          //     label: "",
+          //   ),
+          // ],
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          selectedItemColor: Colors.blue,
         ),
-        const SizedBox(height: 20),
-        DestinationCarousel(),
-      ],
-    )));
+      ),
+    );
   }
 }
--- lib/widgets/hotel_carousel.dart
+++ lib/widgets/hotel_carousel.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:travel_ui/models/destination_model.dart';
 
-class DestinationCarousel extends StatelessWidget {
-  const DestinationCarousel({super.key});
+import '../models/destination_model.dart';
+import '../models/hotel_model.dart';
+
+class HotelCarousel extends StatelessWidget {
+  const HotelCarousel({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +17,7 @@ class DestinationCarousel extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               const Text(
-                "Top Destinations",
+                "Exclusive hotels",
                 style: TextStyle(
                     fontSize: 18,
                     fontWeight: FontWeight.w600,
@@ -40,12 +42,12 @@ class DestinationCarousel extends StatelessWidget {
           height: 300,
           child: ListView.builder(
             scrollDirection: Axis.horizontal,
-            itemCount: destinations.length,
+            itemCount: hotels.length,
             itemBuilder: (_, index) {
-              Destination destination = destinations[index];
+              Hotel hotel = hotels[index];
               return Container(
                 margin: const EdgeInsets.all(10),
-                width: 210,
+                width: 240,
                 child: Stack(
                   alignment: Alignment.topCenter,
                   children: [
@@ -53,7 +55,7 @@ class DestinationCarousel extends StatelessWidget {
                       bottom: 15,
                       child: Container(
                         height: 120,
-                        width: 200,
+                        width: 240,
                         decoration: BoxDecoration(
                             color: Colors.white,
                             borderRadius: BorderRadius.circular(10)),
@@ -61,20 +63,25 @@ class DestinationCarousel extends StatelessWidget {
                           padding: const EdgeInsets.all(10.0),
                           child: Column(
                               mainAxisAlignment: MainAxisAlignment.end,
-                              crossAxisAlignment: CrossAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.center,
                               children: [
                                 Text(
-                                  '${destination.activities.length} activities',
+                                  hotel.name,
                                   style: const TextStyle(
                                       fontSize: 15,
                                       fontWeight: FontWeight.w500,
                                       letterSpacing: 1.2),
                                 ),
                                 Text(
-                                  destination.description,
+                                  hotel.address,
                                   style: const TextStyle(
                                       color: Colors.grey, fontSize: 12),
-                                )
+                                ),
+                                Text("\$${hotel.price} / night",
+                                    style: const TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.w600))
                               ]),
                         ),
                       ),
@@ -89,49 +96,14 @@ class DestinationCarousel extends StatelessWidget {
                                 offset: Offset(0, 4),
                                 blurRadius: 6)
                           ]),
-                      child: Stack(
-                        children: [
-                          ClipRRect(
-                            borderRadius: BorderRadius.circular(20),
-                            child: Image(
-                              height: 180,
-                              width: 180,
-                              image: AssetImage(destination.imageUrl),
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                          Positioned(
-                            left: 10,
-                            bottom: 10,
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  destination.city,
-                                  style: const TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.w500,
-                                      letterSpacing: 1),
-                                ),
-                                Row(
-                                  children: [
-                                    const Icon(
-                                      FontAwesomeIcons.locationArrow,
-                                      size: 10,
-                                      color: Colors.white,
-                                    ),
-                                    const SizedBox(width: 5),
-                                    Text(
-                                      destination.country,
-                                      style: TextStyle(color: Colors.white),
-                                    ),
-                                  ],
-                                )
-                              ],
-                            ),
-                          )
-                        ],
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(20),
+                        child: Image(
+                          height: 180,
+                          width: 220,
+                          image: AssetImage(hotel.imageUrl),
+                          fit: BoxFit.cover,
+                        ),
                       ),
                     )
                   ],
